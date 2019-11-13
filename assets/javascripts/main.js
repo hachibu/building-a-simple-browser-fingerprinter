f23f82f9405ba7058a8dccf81bba58c52d0d1bed
hachibu <raymondsohn@gmail.com>
2019-11-13 10:07:23 +0900

    Initial code commit
    Initial code commit.

---
+++ assets/javascripts/main.js
@@ -0,0 +1,69 @@
+// https://webbjocke.com/javascript-web-encryption-and-hashing-with-the-crypto-api/
+// https://github.com/Valve/fingerprintjs2
+// https://www.browserleaks.com/canvas#how-does-it-work
+
+function supportsCrypto() {
+  return window.crypto && crypto.subtle && window.TextEncoder;
+}
+
+function hex(buff) {
+  return [].map.call(new Uint8Array(buff), b => ('00' + b.toString(16)).slice(-2)).join('');
+}
+
+function hash(algo, str) {
+  return crypto.subtle.digest(algo, new TextEncoder().encode(str));
+}
+
+function fingerprintCanvas(txt) {
+  // https://browserleaks.com/canvas#how-does-it-work
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+
+  canvas.height = 40;
+  canvas.width = 800;
+
+  ctx.textBaseline = 'top';
+  ctx.font = "14px 'Arial'";
+  ctx.textBaseline = 'alphabetic';
+  ctx.fillStyle = '#f60';
+  ctx.fillRect(125,1,40,20);
+  ctx.fillStyle = '#069';
+  ctx.fillText(txt, 2, 15);
+  ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+  ctx.fillText(txt, 4, 17);
+
+  document.body.append(canvas);
+
+  return canvas.toDataURL();
+}
+
+function fingerprint() {
+  var promise = new Promise(function(resolve, reject) {
+    if (!supportsCrypto()) {
+      reject('This browser does not suport the Web Crypto API.');
+    }
+    var components = {
+      userAgent: navigator.userAgent,
+      platform: navigator.platform,
+      language: navigator.language,
+      screenColorDepth: screen.colorDepth,
+      screenAvailHeight: screen.availHeight,
+      screenAvailWidth: screen.availWidth,
+      canvas: fingerprintCanvas('Building a Simple Browser Fingerprinter'),
+    };
+
+    hash('SHA-256', Object.values(components).join('')).then(buffer => {
+      resolve({ hash: hex(buffer), components });
+    });
+  });
+
+  return promise;
+}
+
+fingerprint().then(
+  function({ hash, components }) {
+    console.table(components);
+    fingerprintCanvas(`Your browser fingerprint is ${hash}`);
+  },
+  console.error
+);
